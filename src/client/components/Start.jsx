957d65e27b035d9ec7ba9b0fdb2a16043ef9fa9d
mvb <mvaronabueno@gmail.com>
2019-08-17 18:38:03 +0200

    Adsense auth pending

--- src/client/components/Start.jsx
+++ src/client/components/Start.jsx
@@ -31,11 +31,6 @@ class Start extends Component {
 
     return (
       <div id='connect-area'>
-        <h1>Cartas contra la Humanidad</h1>
-        <img className="hero" alt="Introduction gif" src="https://www.bmsalamanca.com/others/cartascontralahumanidad/hero.gif"/>
-        <Typography paragraph>¡Bienvenido a Cartas contra la Humanidad! Si nunca lo has oído, es un juego en el que un jugador hace una pregunta de una Carta Negra, y todo el mundo responde con sus mejores Cartas Blancas. ¡Cuánto más raro, más risas!<br/><br/>
-        ¡Lo hemos modificado un poco para hacerlo más divertido: pueden jugar desde 3 personas!
-        </Typography>
         <Paper className={classes.paperRoot} elevation={1}>
           <Typography variant='h5' component='h3'>
             Crea una partida o únete a la actual
