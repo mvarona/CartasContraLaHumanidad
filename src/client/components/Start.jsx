4f830fe2baf9e684db2cf6862e6db48302f820d6
mvb <mvaronabueno@gmail.com>
2019-08-18 12:22:41 +0200

    Adsense fix, approval pending

--- src/client/components/Start.jsx
+++ src/client/components/Start.jsx
@@ -6,7 +6,7 @@ import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import AdSense from 'react-adsense';
-
+ 
 const styles = (theme) => ({
   paperRoot: {
     ...theme.mixins.gutters(),
@@ -24,13 +24,18 @@ const styles = (theme) => ({
     height: '56px'
   }
 });
-
+ 
 class Start extends Component {
   render() {
     const { classes } = this.props;
-
+ 
     return (
       <div id='connect-area'>
+        <h1>Cartas contra la Humanidad</h1>
+        <img className="hero" alt="Introduction gif" src="https://www.bmsalamanca.com/others/cartascontralahumanidad/hero.gif"/>
+        <Typography paragraph>¡Bienvenido a Cartas contra la Humanidad! Si nunca lo has oído, es un juego en el que un jugador hace una pregunta de una Carta Negra, y todo el mundo responde con sus mejores Cartas Blancas. ¡Cuánto más raro, más risas!<br/><br/>
+        ¡Lo hemos modificado un poco para hacerlo más divertido: pueden jugar desde 3 personas!
+        </Typography>
         <Paper className={classes.paperRoot} elevation={1}>
           <Typography variant='h5' component='h3'>
             Crea una partida o únete a la actual
@@ -68,5 +73,5 @@ class Start extends Component {
     );
   }
 }
-
+ 
 export default hot(withStyles(styles)(Start));
